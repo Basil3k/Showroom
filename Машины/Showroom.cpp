d46dbdc338421bf1b620b2e2ab96bad7ad5cb1aa
Basil3k <55461619+Basil3k@users.noreply.github.com>
2021-05-04 08:52:48 +0300

    adding comments

--- Машины/Showroom.cpp
+++ Машины/Showroom.cpp
@@ -2,8 +2,8 @@
 #include <iostream>
 
 #include "showroom.h"
-
-//Showroom::~Showroom()
+// uncomment destructor because of we use memory from heap, we should safely return used memory to it
+//Showroom::~Showroom() 
 //{
 //   for (auto it : vehicles)
 //   {
@@ -18,7 +18,7 @@ void Showroom::AddVehicle(Vehicle* vehicle)
 
 void Showroom::SaveAllVehicleToDb()
 {
-   std::ofstream data_base("AllVehicle.txt");
+   std::ofstream data_base("AllVehicle.txt"); // move this variable to common.h and make it constant constexpr
    for (auto it : vehicles)
    {
       data_base << it->VehicleToDb() <<"\n";
@@ -31,20 +31,23 @@ std::string Showroom::FlagFromDb(const std::string& vehicle)//here get only firs
    if (splitted_vehicle.size() > 0)//sometimes we have string which have only symbol of strings end '\n'                          
    {                               //we must compare size compared string for avoidance this situation
       std::string flag = splitted_vehicle.at(0);
-      return flag;
+      return flag; // it could be decreased to "return splitted_vehicle.at(0)"
    }
 }
 
 void Showroom::TakeAllVehicleFromDb()
 {
-   std::fstream data_base("AllVehicle.txt", std::ios::in);
+   // move this variable to common.h and make it constant constexpr
+   std::fstream data_base("AllVehicle.txt", std::ios::in); 
    if (data_base)//need to check whether we opened the database or not
    {
       while (!data_base.eof())
       {
-      std::string boat_line;
+      std::string boat_line; // variable name does not correspond purpose
       std::getline(data_base, boat_line);
       auto flag = FlagFromDb(boat_line);
+      // flag count could be to great and write branch for any flag is not good decision
+      // also move all flags to common file
       if (flag == "b")
       {
          vehicles.push_back(new Boat(boat_line));
@@ -54,7 +57,7 @@ void Showroom::TakeAllVehicleFromDb()
          vehicles.push_back(new Car(boat_line));
       }
       }
-   }
+   } // do somethink in "else" case
 }
 
 void Showroom::PrintAllVehicle()
@@ -71,7 +74,7 @@ bool Showroom::FindVehicle(Vehicle& vehicle)
    //std::cin >> *car;
    for (auto it : vehicles)
    {
-      if (*it == vehicle)
+      if (*it == vehicle) // should be compare car's parameters
       {
          std::cout << "We have vehicle what you find." << std::endl;
          return true;
